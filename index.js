edd55b0566c0deaed6186fe5400584dd30ac0c6d
CHRutledge <47993850+CHRutledge@users.noreply.github.com>
2019-03-20 15:30:17 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log("Yee ha to package")
+
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello world!'));
+
+var arr = [1, 1, 2, 2, 3, 5]
+ 
+newAry = chalk.blue(require("uniq")(arr))
+console.log(chalk.blue(require("uniq")(arr)))
